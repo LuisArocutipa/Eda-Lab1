20f8317e1d8b3ca7e24b1fdf8bfe699c0c7e568d
unknown <larocutipa@unsa.edu.pe>
2023-05-26 15:00:19 -0500

    Se agrego el nombre

--- holaMundo.java
+++ holaMundo.java
@@ -3,7 +3,8 @@ public class holaMundo {
     public static void main(String[]args) {
 
         System.out.println("Hola mundo!");
-        System.out.println("Bienvenido al curso");
+        System.out.print("Bienvenido al curso ");
+        System.out.print("Luis");
 
     }
 
